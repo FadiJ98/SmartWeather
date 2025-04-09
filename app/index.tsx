d21f34785cf185bd6f939d266028f7000f0af019
River Dyke <riverdykemail@gmail.com>
2025-04-09 16:54:22 -0400

    lmao

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react';
-import {StyleSheet, ActivityIndicator, FlatList, Text, View} from 'react-native';
+import {StyleSheet, ActivityIndicator, FlatList, Text, View, Image, ImageBackground, Dimensions} from 'react-native';
 
 const styles = StyleSheet.create({
     thewholeshit: {
@@ -15,22 +15,44 @@ const styles = StyleSheet.create({
     text: {
         fontSize: 30,
     },
+    image: {
+        width:  200,
+        height: 200,
+    },
 });
 
+const options = {
+  enableHighAccuracy: true,
+  timeout: 5000,
+  maximumAge: 0,
+};
+
+function error(err) {
+  console.warn(`ERROR(${err.code}): ${err.message}`);
+}
+
+function success(pos){
+    const crd = pos.coords;
+    console.log(`${crd.latitude}`);
+    console.log(`${crd.longitude}`);
+}
+
 export default function App () {
+    console.log(navigator.geolocation.getCurrentPosition(success,error,options));
     const [data, setData] = useState([]);
 
     const getWeather = async () => {
         try {
             //const response = await fetch('http://dataservice.accuweather.com/forecasts/v1/daily/1day/333697?apikey=ebVzkgYiQZd9KbWc0FLaPc3xx4mU4NCY&language=en-us&details=true&metric=true', {
-            const response = await fetch('http://dataservice.accuweather.com/forecasts/v1/daily/1day/333697?apikey=ebVzkgYiQZd9KbWc0FLaPc3xx4mU4NCY&language=en-us&details=true&metric=false', {
+            const response = await fetch('http://dataservice.accuweather.com/forecasts/v1/hourly/1hour/333697?apikey=ebVzkgYiQZd9KbWc0FLaPc3xx4mU4NCY&language=en-US&details=true&metric=false', {
                 method: 'GET',
             });
           const json = await response.json();
-          console.log(json['DailyForecasts'][0]/* ['Temperature']['Minimum'].Value */);
-          console.log(json['DailyForecasts'][0]/* ['Temperature']['Maximum'].Value */);
-          const temps = [json['DailyForecasts'][0]['Temperature']['Minimum'].Value,json['DailyForecasts'][0]['Temperature']['Maximum'].Value]
-          setData("LOW: ".concat(temps[0], " HIGH: ", temps[1]));
+          console.log(json[0]['Temperature'].Value );
+          console.log(json[0]['Temperature'].Value );
+          const temp = json[0]['Temperature'].Value;
+          const datetime = json[0].DateTime;
+          setData("".concat(temp,"degrees, ",datetime));
         }
         catch (error) {
           console.error(error);
@@ -41,12 +63,15 @@ export default function App () {
         getWeather();
     }, []);
 
+    const logo_width = Dimensions.get('screen').width * 0.4;
+    const logo_height = Dimensions.get('screen').height * 0.4;
     return (
         <View style={styles.thewholeshit}>
-            <View style={styles.container}>
-                <Text style={styles.text}>{data}</Text>
+                  <ImageBackground source={require('../assets/images/sun.png')} style={styles.image}>
+                    <Text>Inside</Text>
+                  </ImageBackground>
+                  <Text style={styles.text}>{data}</Text>
             </View>
-        </View>
     );
 };
 
